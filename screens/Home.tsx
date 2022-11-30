40fd2dc69517e6334b584b1825b81312fe7738e8
sarem eskandary <38113700+saremeskandary@users.noreply.github.com>
2022-11-30 15:12:09 +0330

    formik added and tested

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,14 +1,45 @@
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
+import { Button, TextInput } from "react-native";
 import { RootTabScreenProps } from "../types";
+import React from "react";
+import { Formik } from "formik";
 
 export default function Home({ navigation }: RootTabScreenProps<"Home">) {
   return (
-    <View className="bg-red-700 p-10">
-      <Text >Home</Text>
-      
-      <EditScreenInfo path="/screens/Home.tsx" />
-    </View>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View className="w-full h-full items-center ">
+          <View className="gap-4 max-w-xl m-4">
+            <View className="shadow-xl">
+              <TextInput
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                keyboardType={"email-address"}
+              />
+            </View>
+            <View className="shadow-xl">
+              <TextInput
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                keyboardType={"visible-password"}
+              />
+            </View>
+            <View className="shadow-xl">
+              <Button
+                onPress={() => {
+                  handleSubmit();
+                }}
+                title="Submit"
+              />
+            </View>
+          </View>
+        </View>
+      )}
+    </Formik>
   );
 }
-
